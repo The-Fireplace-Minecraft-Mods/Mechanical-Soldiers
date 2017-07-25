19533f5e8117ebc76f72f3c8ec0e4786ad7657d5
The_Fireplace <the.f1repl4ce@gmail.com>
2017-07-24 21:22:50 -0500

    Add 2 metal melt recipes

--- src/main/java/the_fireplace/mechsoldiers/registry/MechCraftingRecipes.java
+++ src/main/java/the_fireplace/mechsoldiers/registry/MechCraftingRecipes.java
@@ -1,5 +1,7 @@
 package the_fireplace.mechsoldiers.registry;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import the_fireplace.mechsoldiers.MechSoldiers;
 import the_fireplace.overlord.registry.CraftingRecipes;
@@ -10,11 +12,14 @@ import the_fireplace.overlord.registry.CraftingRecipes;
 public class MechCraftingRecipes {
 	public static ItemStack robot_constructor = new ItemStack(MechSoldiers.robot_constructor);
 	public static ItemStack wood_skeleton = new ItemStack(MechSoldiers.skeleton_wood);
+	public static ItemStack iron_skeleton = new ItemStack(MechSoldiers.skeleton_iron);
+	public static ItemStack gold_skeleton = new ItemStack(MechSoldiers.skeleton_gold);
 
 	public static void register() {
 		CraftingRecipes.addRecipe(robot_constructor, " d ", "drd", "ddd", 'd', "gemDiamond", 'r', CraftingRecipes.skeleton_maker);
 		CraftingRecipes.addRecipe(wood_skeleton, "sls", "sss", "sss", 's', "stickWood", 'l', "logWood");
 
-		
+		MetalMeltRecipes.instance().addMeltingRecipe(iron_skeleton, "ingotIron", "blockIron");
+		MetalMeltRecipes.instance().addMeltingRecipe(gold_skeleton, "ingotGold", "blockGold");
 	}
 }
