703781aa4bd88d01aa8ef1e0c4857235df599474
The_Fireplace <the.f1repl4ce@gmail.com>
2017-08-16 17:58:14 -0500

    Prep for next Guide API update

--- src/main/java/the_fireplace/mechsoldiers/compat/guide/MechSoldiersGuideCompat.java
+++ src/main/java/the_fireplace/mechsoldiers/compat/guide/MechSoldiersGuideCompat.java
@@ -40,7 +40,7 @@ public class MechSoldiersGuideCompat implements IGuideBook {
 
 		List<IPage> pages = Lists.newArrayList();
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.1.1")));
-		//pages.add(new PageIRecipe(new ShapedOreRecipe(MechSoldiers.robot_constructor, " d ", "drd", "ddd", 'd', "gemDiamond", 'r', CraftingRecipes.skeleton_maker)));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "robot_constructor")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.1.2")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.1.3")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.1.4")));
@@ -48,29 +48,29 @@ public class MechSoldiersGuideCompat implements IGuideBook {
 
 		pages = Lists.newArrayList();
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.2.1")));
-		//pages.add(new PageIRecipe(new ShapedOreRecipe(MechSoldiers.metal_part_constructor, "ifi", "iti", 'i', "ingotIron", 'f', MechCraftingRecipes.furnace, 't', MechCraftingRecipes.mini_tank)));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "metal_part_constructor")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.2.2")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.2.3")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.2.4")));
-		//pages.add(new PageIRecipe(new ShapedOreRecipe(MechSoldiers.skeleton_wood, "sls", "sss", "sss", 's', "stickWood", 'l', "logWood")));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "skeleton_wood")));
 		entries.put(new ResourceLocation(MechSoldiers.MODID, "1.2"), new EntryItemStack(pages, proxy.translateToLocal("mechsoldiers.guide.1.2"), new ItemStack(MechSoldiers.metal_part_constructor)));
 
 		pages = Lists.newArrayList();
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.3.1")));
-		//pages.add(new PageIRecipe(new ShapedOreRecipe(MechSoldiers.cpu_melter, "ift", "ibi", "tfi", 'i', "ingotIron", 'f', MechCraftingRecipes.furnace, 't', MechCraftingRecipes.mini_tank, 'b', MechCraftingRecipes.blueprint)));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "cpu_melter")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.3.2")));
 		entries.put(new ResourceLocation(MechSoldiers.MODID, "1.3"), new EntryItemStack(pages, proxy.translateToLocal("mechsoldiers.guide.1.3"), new ItemStack(MechSoldiers.cpu_melter)));
 
 		pages = Lists.newArrayList();
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.4.1")));
-		//pages.add(new PageIRecipe(new ShapedOreRecipe(MechSoldiers.mini_tank, "ggg", "g g", "ggg", 'g', MechCraftingRecipes.glass_pane)));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "mini_tank")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.4.2")));
-		//pages.add(new PageIRecipe(new ShapelessOreRecipe(MechCraftingRecipes.blueprint2, MechCraftingRecipes.blueprint, "paper", "dyeBlue", "dyeWhite", "nuggetIron")));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "blueprint")));
 		entries.put(new ResourceLocation(MechSoldiers.MODID, "1.4"), new EntryItemStack(pages, proxy.translateToLocal("mechsoldiers.guide.1.4"), new ItemStack(MechSoldiers.blueprint)));
 
 		pages = Lists.newArrayList();
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.5.1")));
-		//pages.add(new PageIRecipe(new ShapedOreRecipe(MechSoldiers.part_stainer, "sbb", "sib", "sbb", 's', "stone", 'i', "ingotIron", 'b', Blocks.STONEBRICK)));
+		//pages.add(new PageJsonRecipe(new ResourceLocation(MechSoldiers.MODID, "part_stainer")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.5.2")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.5.3")));
 		pages.add(new PageText(proxy.translateToLocal("mechsoldiers.guide.1.5.4")));
